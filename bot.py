0e1f536c61c682f64782b1ee950f6e762fa0fdbd
Lucasch1 <lucasch2011@gmail.com>
2023-07-25 17:36:46 -0300

    Att

--- bot.py
+++ bot.py
@@ -8,13 +8,28 @@ print('2 - Verificar SMA de um ativo')
 op = input('Opção: ')
 
 if op == '1':
-    tkr1 = input('Ativo 1: ')
-    tkr2 = input('Ativo 2: ')
-    tkr1 = tkr1.upper() + '.SA'
-    tkr2 = tkr2.upper() + '.SA'
-    dat1 = getData(tkr1, '2019-01-01', '2023-07-24')
-    dat2 = getData(tkr2, '2019-01-01', '2023-07-24')
-    corr = getCorrel(dat1, dat2)
+    print('Os ativos sao acoes brasileras?')
+    print('1 - Sim')
+    print('2 - Nao')
+    op = input('Opção: ')
+    if op == '1':
+        tkr1 = input('Ativo 1: ')
+        tkr2 = input('Ativo 2: ')
+        tkr1 = tkr1.upper() + '.SA'
+        tkr2 = tkr2.upper() + '.SA'
+        dat1 = getData(tkr1, '2019-01-01', '2023-07-24')
+        dat2 = getData(tkr2, '2019-01-01', '2023-07-24')
+        corr = getCorrel(dat1, dat2)
+    elif op == '2':
+        print('Digite o codigo do ativo exatamente como encontrado no Yahoo Finance')
+        tkr1 = input('Ativo 1: ')
+        tkr2 = input('Ativo 2: ')
+        tkr1 = tkr1.upper()
+        tkr2 = tkr2.upper()
+        dat1 = getData(tkr1, '2019-01-01', '2023-07-24')
+        dat2 = getData(tkr2, '2019-01-01', '2023-07-24')
+        corr = getCorrel(dat1, dat2)
+
     print(f'A correlação entre {tkr1} e {tkr2} é de {corr}')
 
 elif op == '2':
