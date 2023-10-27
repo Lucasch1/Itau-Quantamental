eb8d45ac1a79649551722ba3b0d84b5b9a45feaa
Lucasch1 <lucasch2011@gmail.com>
2023-10-26 21:34:26 -0300

    finalizacao backtest

--- src/backtest.py
+++ src/backtest.py
@@ -24,32 +24,34 @@ def pair_trading_strategy(data, pair_file, ratio_file):
 
     # Initialize signals Series
     signals = pd.DataFrame(index=data["Date"])
-    print(signals)
+    # print(signals)
 
     # Loop over pairs
     for i, row in pairs.iterrows():
         # Extract pair data
         stock1, stock2 = row["Par"].split("/")
         ratio_mean = ratios[stock1 + "/" + stock2].mean()
-        ratio_std = ratios[stock1 + "/" + stock2].std()
+        # ratio_std = ratios[stock1 + "/" + stock2].std()
 
         # Calculate ratio
-        ratio = data[stock1] / data[stock2]
+        # ratio = data[stock1] / data[stock2]
+        # print("ratio:", ratio)
 
         # Calculate z-score of ratio
-        z_score = (ratio - ratio_mean) / ratio_std
+        # z_score = (ratio - ratio_mean) / ratio_std
 
         # Generate signals based on z-score and max exposition
         lis = []
-        for z in z_score:
-            if z > 0.05:
+        for z in ratios[stock1 + "/" + stock2]:
+            if z > (1.05 * ratio_mean):
                 lis.append(1)
-            elif z == 0.00:
+            elif (ratio_mean * 1.01) > z > (0.99 * ratio_mean):
                 lis.append(0)
-            elif z < -0.05:
+            elif z < (0.95 * ratio_mean):
                 lis.append(-1)
             else:
-                lis.append("NaN")
+                # print(z)
+                lis.append(2)
         signals[f"{stock1}/{stock2}"] = lis
 
     return signals
@@ -71,72 +73,123 @@ def backtest(data, strategy, pair_file, ratio_file, max_exposition):
     """
     # Calculate signals using the strategy
     signals = strategy
+    print(signals.head(100))
+    resultados = []
 
     for i in pair_file["Par"]:
         stock1, stock2 = i.split("/")
         sig = signals[f"{stock1}/{stock2}"]
         status_LS = {
+            "Name": f"{stock1}/{stock2}",
             "stock1": stock1,
             "stock2": stock2,
             "status": False,
             "status_1": False,
             "status_-1": False,
-            "exposition": 0,
-            "returns": 0,
-            "stokc1_buy_price": [],
+            "exposition_stock1": 0,
+            "exposition_stock2": 0,
+            "returns_total": 0,
+            "stock1_buy_price": [],
             "stock2_buy_price": [],
             "stock1_sell_price": [],
             "stock2_sell_price": [],
+            "returns": [],
+            "returns_Dates": [],
+            "ls_counter": 0,
         }
+        
+        for i, j in signals.iterrows():
+            if j[f"{stock1}/{stock2}"] == 1 and status_LS["status"] == False:
+                # print(data.loc[data["Date"] == i, stock1])
+                if float(data.loc[data["Date"] == i, stock1]) == 0.00 or float(data.loc[data["Date"] == i, stock2]) == 0.00:
+                    print("zerou")
+                else:
+                    status_LS["status"] = True
+                    status_LS["status_1"] = True
+                    status_LS["stock1_sell_price"].append(float(data.loc[data["Date"] == i, stock1]))
+                    status_LS["stock2_buy_price"].append(float(data.loc[data["Date"] == i, stock2]))
 
-        for i, j in sig.index, sig:
-            if j == 1 and status_LS["status"] == False:
-                status_LS.update({"status": True})
-                status_LS["stock1_sell_price"].append(data[stock1][i])
-                status_LS["stock2_buy_price"].append(data[stock2][i])
-                status_LS["status_1"] = True
-            elif j == 0 and status_LS["status"] == True:
+                    # LOGICA DE EXPOSICAO E LOGICA DE RETURNS
+                    status_LS["exposition_stock1"] = -100.00 * float(data.loc[data["Date"] == i, stock1])
+                    status_LS["exposition_stock2"] = 100.00 * float(data.loc[data["Date"] == i, stock2])
+                    status_LS["returns_Dates"].append([str(i)])
+
+
+            elif j[f"{stock1}/{stock2}"] == 0 and status_LS["status"] == True:
                 if status_LS["status_1"] == True:
-                    status_LS.update({"status": False})
-                    status_LS["stock1_buy_price"].append(data[stock1][i])
-                    status_LS["stock2_sell_price"].append(data[stock2][i])
+                    #Precos de compra e venda das acoes
+                    status_LS["stock1_buy_price"].append(float(data.loc[data["Date"] == i, stock1]))                 
+                    status_LS["stock2_sell_price"].append(float(data.loc[data["Date"] == i, stock2]))
+
+                    # LOGICA DE EXPOSICAO E LOGICA DE RETURNS
+                    profit_stock1 = status_LS["exposition_stock1"] + 100 * float(data.loc[data["Date"] == i, stock1])
+                    profit_stock2 = status_LS["exposition_stock2"] - 100 * float(data.loc[data["Date"] == i, stock1])
+                    profit = profit_stock1 + profit_stock2
+                    status_LS["returns_total"] = status_LS["returns_total"] + profit
+                    status_LS["returns"].append(profit)
+                    status_LS["exposition_stock1"] = 0
+                    status_LS["exposition_stock2"] = 0
+                    
+                    #Conclusao
                     status_LS["status_1"] = False
+                    status_LS["status"] = False
+                    status_LS["returns_Dates"][int(status_LS["ls_counter"])].append(str(i))
+                    status_LS["ls_counter"] = status_LS["ls_counter"] + 1
+                    
+
+
                 elif status_LS["status_-1"] == True:
-                    status_LS.update({"status": False})
-                    status_LS["stock1_sell_price"].append(data[stock1][i])
-                    status_LS["stock2_buy_price"].append(data[stock2][i])
+                    #Precos de compra e venda das acoes
+                    status_LS["stock1_sell_price"].append(float(data.loc[data["Date"] == i, stock1]))
+                    status_LS["stock2_buy_price"].append(float(data.loc[data["Date"] == i, stock2]))
+                    
+                    # LOGICA DE EXPOSICAO E LOGICA DE RETURNS
+                    profit_stock1 = status_LS["exposition_stock1"] - 100 * float(data.loc[data["Date"] == i, stock1])
+                    profit_stock2 = status_LS["exposition_stock2"] + 100 * float(data.loc[data["Date"] == i, stock1])
+                    profit = profit_stock1 + profit_stock2
+                    status_LS["returns_total"] = status_LS["returns_total"] + profit
+                    status_LS["returns"].append(profit)
+                    status_LS["exposition_stock1"] = 0
+                    status_LS["exposition_stock2"] = 0
+
+                    #Conclusao
                     status_LS["status_-1"] = False
-            elif j == -1 and status_LS["status"] == False:
-                status_LS.update({"status": True})
-                status_LS["stock1_buy_price"].append(data[stock1][i])
-                status_LS["stock2_sell_price"].append(data[stock2][i])
-                status_LS["status_-1"] = True
+                    status_LS["status"] = False
+                    status_LS["returns_Dates"][status_LS["ls_counter"]].append(str(i))
+                    status_LS["ls_counter"] = status_LS["ls_counter"] + 1
 
-    # Calculate returns based on the signals
-    returns = signals.shift(1) * data["returns"]
+                    # LOGICA DE EXPOSICAO E LOGICA DE RETURNS
 
-    # Calculate cumulative returns
-    cumulative_returns = (1 + returns).cumprod()
+            elif j[f"{stock1}/{stock2}"] == -1 and status_LS["status"] == False:
+                if int(data.loc[data["Date"] == i, stock1]) == 0 or int(data.loc[data["Date"] == i, stock2]) == 0:
+                    print("zerou")
+                else:
+                    status_LS["status"] = True
+                    status_LS["stock1_buy_price"].append(float(data.loc[data["Date"] == i, stock1]))
+                    status_LS["stock2_sell_price"].append(float(data.loc[data["Date"] == i, stock2]))
+                    status_LS["status_-1"] = True
 
-    # Calculate drawdowns
-    max_returns = cumulative_returns.cummax()
-    drawdowns = (cumulative_returns - max_returns) / max_returns
+                    status_LS["status"] = True
+                    status_LS["status_-1"] = True
+                    status_LS["stock1_buy_price"].append(float(data.loc[data["Date"] == i, stock1]))
+                    status_LS["stock2_sell_price"].append(float(data.loc[data["Date"] == i, stock2]))
 
-    # Combine results into a DataFrame
-    results = pd.DataFrame(
-        {
-            "signals": signals,
-            "returns": returns,
-            "cumulative_returns": cumulative_returns,
-            "drawdowns": drawdowns,
-        }
-    )
+                    # LOGICA DE EXPOSICAO E LOGICA DE RETURNS
+                    status_LS["exposition_stock1"] = 100.00 * float(data.loc[data["Date"] == i, stock1])
+                    status_LS["exposition_stock2"] = -100.00 * float(data.loc[data["Date"] == i, stock2])
+                    status_LS["returns_Dates"].append([str(i)])
 
-    return results
+
+
+        resultados.append(status_LS)
+    print(resultados[0])
+
+
+    return resultados
 
 
 def main():
-    data = pd.read_csv("src/database/mkt_data.csv")
+    data = pd.read_csv("src/database/mkt_data.csv", sep=",")
     pair_file_path = "src/output/MeanReverting.csv"
     pair_file = pd.read_csv(pair_file_path, sep=";")
     ratio_file_path = "src/output/ratioPares.csv"
@@ -144,11 +197,11 @@ def main():
     exposition = 1000000.00
 
     signals = pair_trading_strategy(data, pair_file_path, ratio_file_path)
-    print(signals)
+    # print(signals)
 
     test = backtest(data, signals, pair_file, ratio_file, exposition)
 
-    print(test)
+    # print(test)
 
 
 if __name__ == "__main__":
